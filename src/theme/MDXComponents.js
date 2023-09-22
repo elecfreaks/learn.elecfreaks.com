a3836b057c9903f9cb8a3c1f489ac9af96e04a7b
zhenping <wangzhenping1994@gmail.com>
2023-09-22 12:17:19 +0800

    Apple: 123

--- src/theme/MDXComponents.js
+++
@@ -1,9 +0,0 @@
-import MDXComponents from '@theme-original/MDXComponents';
-import Card from '@site/src/components/Card';
-import Cardbox from '@site/src/components/Cardbox';
-
-export default {
-    ...MDXComponents,
-    card: Card,
-    cardbox: Cardbox,
-};

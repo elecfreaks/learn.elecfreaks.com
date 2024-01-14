78ec665c5a14cffe1700b7320a5af698321ed1f7
yichang <740123730@qq.com>
2024-01-15 01:59:31 +0800

    Create MDXComponents.js

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,9 @@
+import MDXComponents from '@theme-original/MDXComponents';
+import Card from '@site/src/components/Card';
+import Cardbox from '@site/src/components/Cardbox';
+
+export default {
+    ...MDXComponents,
+    card: Card,
+    cardbox: Cardbox,
+};
